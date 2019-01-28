3e06b8956704dc44d508429771be6a4c3a8b2c87
srdoyen <srdoyen@gmail.com>
2019-01-27 22:37:23 -0800

    First Post
    - First Fullstack operation
    - Post product in postman

--- model/wishlist.js
+++ model/wishlist.js
@@ -8,4 +8,4 @@ var wishList = new Schema({
     products:[{type: ObjectId, ref:'Product'}]
 });
 
-modulce.exports = mongoose.model('WishList',wishList);
+module.exports = mongoose.model('WishList',wishList);
--- server.js
+++ server.js
@@ -11,6 +11,22 @@ var WishList = require('./model/wishList');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:false}));
 
+app.post('/product', function(request, response) {
+    var product = new Product();
+    //var product = new Product(request.body);
+    product.title = request.body.title;
+    product.price = request.body.price;
+    product.save(function(err, savedProduct){
+        if(err) {
+            response.status(500).send({error: "Could not save product"});
+        }
+        else {
+            response.status(200).send(savedProduct);
+        }
+    });
+});
+
+
 app.listen(3000, function() {
     console.log("Swap Shop API running on port 3000...");
 });
